1ffc5eed0f6bdf84223331c1c4eedc8ae33fbd0d
DiogoMMC <48791368+DiogoMMC@users.noreply.github.com>
2020-09-02 01:33:35 +0100

    added views navBar ementa admin  and anded amdin and ementa routes

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var adminRouter = require('./routes/admin');
 var usersRouter = require('./routes/users');
-
+var homeRouter = require('./routes/home');
+var ementaRouter = require('./routes/ementa');
 var app = express();
 
 // view engine setup
@@ -19,8 +20,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', homeRouter);
 app.use('/users', usersRouter);
+app.use('admin',adminRouter);
+app.use('/ementa',ementaRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/home.js
+++ routes/home.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('HOME PAGE');
+  res.render('home');
   
 });
 
