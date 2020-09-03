a066c3e30855496c71ee76a14cc0bea73ee94cca
DiogoMMC <48791368+DiogoMMC@users.noreply.github.com>
2020-09-03 16:33:55 +0100

    updated admin views

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('admin', { title: 'Express' });
+  res.render('admin');
 });
+
+router.get('/ementa', function(req, res, next) {
+  res.render('./adminViews/adminEmenta');
+});
+
+router.get('/clients', function(req, res, next) {
+  res.render('./adminViews/adminClients');
+});
+
+router.get('/reserves', function(req, res, next) {
+  res.render('./adminViews/adminReserves');
+});
+
+
 
 module.exports = router;
